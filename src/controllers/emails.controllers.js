47c7cf689ed501e8352e1e5b3e58b2bd269e61a8
Kendy <kendyelisca5@gmail.com>
2023-10-11 12:25:26 -0500

    Changed email style

--- src/controllers/emails.controllers.js
+++ src/controllers/emails.controllers.js
@@ -13,16 +13,37 @@ const send = catchError(async (req, res) => {
 });
 const contact = catchError(async (req, res) => {
   const { name, email, message } = req.body;
+  const emailContent = `
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f4f4f4;
+        }
+        h1 {
+          color: #333;
+        }
+        .message {
+          font-size: 16px;
+          margin-top: 20px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>${name}</h1>
+      <h2>With the email: <strong>${email}</strong></h2>
+      <h2>Has seen your portfolio</h2>
+      <p class="message">${message}</p>
+    </body>
+  </html>
+`;
   await sendEmail({
     to: "kendyelisca5@gmail.com",
     subject: "Message from your portfolio",
-    html: `<h1>${name}</h1>
-    <h2>with the email:
-    <span><strong>${email}</strong></span></h2>
-    <h2>Has seen your portfolio</h2>
-    <p>${message}<p>`,
+    html: emailContent,
   });
-  return res.json({ message: "email sent" });
+  return res.json({ message: "Email sent" });
 });
 
 module.exports = {
